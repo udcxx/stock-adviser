0d6809599343f017340c5a43371116324652c0e0
d.uezu <>
2020-09-16 00:10:22 +0900

    #2 スクレイピングの準備だけ

--- src/dataupdate.php
+++ src/dataupdate.php
@@ -5,4 +5,8 @@
     //                      Ver 1.0.0
     // ------------------------------
 
+    require_once("./assets/phpQuery-onefile.php");
+    $html = file_get_contents("");
+    echo phpQuery::newDocument($html)->find("")->text();
+
 ?>
