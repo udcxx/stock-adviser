c38af2fd58450ef2c6fcd757ef912c15a3a30714
udcxx <me@udcxx.me>
2020-10-06 08:43:14 +0900

    #2 株価取得できるように

--- src/dataupdate.php
+++ src/dataupdate.php
@@ -5,8 +5,13 @@
     //                      Ver 1.0.0
     // ------------------------------
 
-    require_once("./assets/phpQuery-onefile.php");
-    $html = file_get_contents("");
-    echo phpQuery::newDocument($html)->find("")->text();
+
+    echo getStockPrice("7203");
+
+    function getStockPrice ($stockCode) {
+        require_once("./assets/phpQuery-onefile.php");
+        $html = file_get_contents("https://kabuoji3.com/stock/".$stockCode."/");
+        return phpQuery::newDocument($html) -> find("tbody") -> find("td:eq(6)") -> text();
+    }
 
 ?>
